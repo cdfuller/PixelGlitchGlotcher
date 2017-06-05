4917c19dc217ecf07fa63aec013692a720a697a8
Cody Fuller <git@codyfuller.com>
2017-06-04 20:19:22 -0500

    Move canvas generation to CanvasAccess.js

--- CanvasAccess.js
+++ CanvasAccess.js
@@ -79,3 +79,82 @@ function sortAllRows(){
   }
   console.log("Sorted All Rows");
 }
+
+function generateCanvas(){
+  console.log('Generating canvas');
+  for (var y = 0; y < height; y++){
+    for (var x = 0; x < width; x++){
+      switch(config.canvasStart){
+        case 'HSB':
+          var sat = 80;
+          var brt = 80;
+          var c = color(`hsb(${int(random(360))}, ${sat}%, ${brt}%)`);
+          setPixelColor(x, y, c);
+          break;
+        case 'RGB':
+          setPixelColor(x, y, [random(100, 255), random(100, 255), random(100, 255), 255]);     
+          break;
+        case 'Red':
+          setPixelColor(x, y, [random(0, 255), 0, 0, 255]);
+          break;
+        case 'Green':
+          setPixelColor(x, y, [0, random(0, 255), 0, 255]);
+          break;
+        case 'Blue':
+          setPixelColor(x, y, [0, 0, random(0, 255), 255]);
+          break;
+        case 'RedGreen':
+          setPixelColor(x, y, [random(0, 255), random(0, 50), 0, 255]);
+          break;
+        case 'Custom RGB':
+          var r, g, b;
+
+          if (config.minA == config.maxA){
+            r = config.minA;
+          } else {
+            r = int(random(config.minA, config.maxA));
+          }
+
+          if (config.minB == config.maxB){
+            g = config.minB;
+          } else {
+            g = int(random(config.minB, config.maxB));
+          }
+
+          if (config.minC == config.maxC){
+            b = config.minC;
+          } else {
+            b = int(random(config.minC, config.maxC));
+          }
+
+          setPixelColor(x, y, [r, g, b, 255]);
+          break;
+        case 'Custom HSB':
+          var h = int(random(config.minA, config.maxA));
+          var s = int(random(config.minB, config.maxB));
+          var b = int(random(config.minC, config.maxC));
+          var c = color(`hsb(${h}, ${s}%, ${b}%)`);
+          setPixelColor(x, y, c)
+          break;
+        default:
+          // Set an ugly brown if canvasStart preset doesn't match
+          setPixelColor(x, y, color('brown'));
+      }
+    }
+  }
+  console.log("Canvas finished generating");
+}
+
+function renderCanvas(){
+  if (config['canvasStart'] == 'Image'){
+    var canvas = createCanvas(img.width, img.height);
+    canvas.parent('sketch-container');
+    pixelDensity(1);
+    image(img, 0, 0);
+  } else {
+    var canvas = createCanvas(640, 640);
+    canvas.parent('sketch-container');
+    pixelDensity(1);
+    generateCanvas();
+  }
+}
