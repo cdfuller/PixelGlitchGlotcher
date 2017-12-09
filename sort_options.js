eb9e1cf12bf495afdf30c53b3cb435994572b472
Cody Fuller <git@codyfuller.com>
2017-12-08 23:27:32 -0500

    Cleaning up variable names

--- sort_options.js
+++ sort_options.js
@@ -84,14 +84,14 @@ SORT_MODES = {
 // Taken from p5js
 // Original: p5.ColorConversion._rgbaToHSBA
 function getHue(rgba) {
-  var red = rgba[0];
-  var green = rgba[1];
-  var blue = rgba[2];
+  let red = rgba[0];
+  let green = rgba[1];
+  let blue = rgba[2];
 
-  var val = Math.max(red, green, blue);
-  var chroma = val - Math.min(red, green, blue);
+  let val = Math.max(red, green, blue);
+  let chroma = val - Math.min(red, green, blue);
 
-  var hue, sat;
+  let hue, sat;
   if (chroma === 0) {  // Return early if grayscale.
     hue = 0;
     sat = 0;
@@ -117,14 +117,14 @@ function getHue(rgba) {
 };
 
 function getSaturation(rgba) {
-  var red = rgba[0];
-  var green = rgba[1];
-  var blue = rgba[2];
+  let red = rgba[0];
+  let green = rgba[1];
+  let blue = rgba[2];
 
-  var val = Math.max(red, green, blue);
-  var chroma = val - Math.min(red, green, blue);
+  let val = Math.max(red, green, blue);
+  let chroma = val - Math.min(red, green, blue);
 
-  var sat;
+  let sat;
   if (chroma === 0) {  // Return early if grayscale.
     sat = 0;
   }
@@ -136,44 +136,44 @@ function getSaturation(rgba) {
 };
 
 function getBrightness(rgba) {
-  var red = rgba[0];
-  var green = rgba[1];
-  var blue = rgba[2];
+  let red = rgba[0];
+  let green = rgba[1];
+  let blue = rgba[2];
 
-  var val = Math.max(red, green, blue);
+  let val = Math.max(red, green, blue);
 
   return val;
 };
 
 function getLightness(rgba) {
-  var red = rgba[0];
-  var green = rgba[1];
-  var blue = rgba[2];
+  let red = rgba[0];
+  let green = rgba[1];
+  let blue = rgba[2];
 
-  var val = Math.max(red, green, blue);
-  var min = Math.min(red, green, blue);
-  var li = (val + min) / 2;
+  let val = Math.max(red, green, blue);
+  let min = Math.min(red, green, blue);
+  let li = (val + min) / 2;
 
   return li;
 }
 
-function getLuminance(c){
-  return 0.299*c[0] + 0.587*c[1] + 0.114*c[2];
+function getLuminance(rgba) {
+  return 0.299*rgba[0] + 0.587*rgba[1] + 0.114*rgba[2];
 }
 
-function getChroma(c){
-  var red = c[0];
-  var blue = c[1];
-  var green = c[2];
+function getChroma(rgba) {
+  let red = rgba[0];
+  let blue = rgba[1];
+  let green = rgba[2];
 
-  var val = Math.max(red, green, blue);
-  var chroma = val - Math.min(red, green, blue);
+  let val = Math.max(red, green, blue);
+  let chroma = val - Math.min(red, green, blue);
 
   return chroma;
 }
 
-function getHashV1(c) {
-  let s = c[0].toString() + c[1].toString() + c[2].toString();
+function getHashV1(rgba) {
+  let s = rgba[0].toString() + rgba[1].toString() + rgba[2].toString();
   let hash = 0;
 
   for (let i = 0; i < s.length; i++) {
@@ -184,9 +184,9 @@ function getHashV1(c) {
   return hash;
 }
 
-function getHashV2(c) {
-  let str = c[0].toString() + c[1].toString() + c[2].toString();
-  var hash = 5381,
+function getHashV2(rgba) {
+  let str = rgba[0].toString() + rgba[1].toString() + rgba[2].toString();
+  let hash = 5381,
       i    = str.length;
 
   while(i) {
